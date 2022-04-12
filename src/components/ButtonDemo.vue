91cf9516bf3e418613ace37773b53fc6e9f08c00
btbrad <biantaobrad@gmail.com>
2022-04-12 16:28:22 +0800

    feat: button组件basic

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    默认：
+    <guru-button @click="onClick">button</guru-button>
+    <br />
+    尺寸size:
+    small: <guru-button @click="onClick" size="small">button</guru-button>
+    <br />
+    类型type：
+    link: <guru-button @click="onClick" type="link">button</guru-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import GuruButton from '@/lib/Button.vue'
+
+export default defineComponent({
+  name: 'Button-Demo',
+  components: {
+    GuruButton
+  },
+  setup () {
+    const onClick = () => {
+      alert('clicked!')
+    }
+    return {
+      onClick
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/lib/Button.vue
@@ -0,0 +1,61 @@
+<template>
+  <button class="guru-button" :class="classes">
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  name: 'Guru-Button',
+  props: {
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    type: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  setup (props) {
+    const { size, type } = props
+    const classes = computed(() => {
+      return {
+        [`guru-${size}`]: size,
+        [`guru-${type}`]: type
+      }
+    })
+    return {
+      classes
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.guru-button {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover {
+    background: #0f0;
+    color: #fff;
+    border-color: #fff;
+  }
+  &.guru-small {
+    font-size: 14px;
+    padding: 3px;
+  }
+  &.guru-link {
+    border: none;
+    background: none;
+    &:hover {
+      color: #0f0;
+    }
+  }
+}
+</style>
